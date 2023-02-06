cb2ceff0ade614f68d16b3e8caf08a01242536cc
BenitoMF <b.martinez2@sapalomera.cat>
2023-02-06 16:01:06 +0100

    Prueba Benito

---
+++ src/proves/Main.java
@@ -0,0 +1,36 @@
+package proves;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileReaderClass reader = new FileReaderClass();
+        List<String[]> data = reader.readFile("C:/M02/02201606_MESA/07021606.DAT");
+
+        DatabaseInserter inserter = new DatabaseInserter();
+        inserter.insertData(data);
+    }
+
+}
+/*public class DatabaseInserter {
+     public void insertData(List<String[]> data) {
+
+                  for (String[] values : data) {
+                    pstmt.setString(1, values[0]);
+                    pstmt.setString(2, values[1]);
+                    pstmt.setString(3, values[2]);
+                    pstmt.executeUpdate();
+                }
+            }
+            }
+            */
+
+/*
+public class FileReaderClass {
+
+  public static List<String[]> readFile(String fileName) {
+    List<String[]> data = new ArrayList<>();
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(fileName));
+      String line;
+ */
