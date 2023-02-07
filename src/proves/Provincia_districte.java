6dfa1fd4569476b6a169501b2b171edef68524ef
AlbeXx010 <a.morcillo@sapalomera.cat>
2023-02-07 13:42:04 +0100

    Prueba Alberto

---
+++ src/proves/Provincia_districte.java
@@ -0,0 +1,43 @@
+package proves;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
+import java.util.List;
+
+public class Provincia_districte {
+
+    public void insertData(String nom, String codi_ine){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://<IP>/eleccions2017", "perepi", "pastanaga");
+
+
+            //Preparem el Date
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+            // the mysql insert statement
+            String query = " INSERT INTO municipis (nom, codi_ine)"
+                    + " values (?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+                preparedStmt.setString(1,nom);
+                preparedStmt.setString(2,codi_ine);
+                //preparedStmt.setDate(6, startDate);
+                /*preparedStmt.setString(7, "IT_PROG");
+                preparedStmt.setFloat(8, 5000.12f);*/
+
+                // execute the preparedstatement
+                preparedStmt.execute();
+            //Tanquem la connexió
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
