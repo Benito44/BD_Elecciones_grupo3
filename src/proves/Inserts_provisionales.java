7e36113e06246b01da9fed717791437c8ebe228f
BenitoMF <b.martinez2@sapalomera.cat>
2023-02-14 13:08:34 +0100

    Prueba Benito

--- src/proves/Inserts_provisionales.java
+++ src/proves/Inserts_provisionales.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Inserts_provisionales {
 
@@ -63,4 +64,36 @@ public class Inserts_provisionales {
             System.out.println(e);
         }
     }
+    public static void insertPersones(String nom, String cog1, String cog2, String sexe, Date data_naixement,String dni) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://<IP>/eleccions2017", "perepi", "pastanaga");
+
+
+            //Preparem el Date
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+            // the mysql insert statement
+            String query = " INSERT INTO persones (nom,cog1,cog2,sexe,data_naixement,dni)"
+                    + " values (?, ?, ?, ?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, nom);
+            preparedStmt.setString(2, cog1);
+            preparedStmt.setString(3, cog2);
+            preparedStmt.setString(4, sexe);
+            preparedStmt.setDate(5, (java.sql.Date) data_naixement);
+            preparedStmt.setString(6, dni);
+            // execute the preparedstatement
+            preparedStmt.execute();
+            //Tanquem la connexió
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
